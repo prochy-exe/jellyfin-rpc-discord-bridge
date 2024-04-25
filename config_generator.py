5324e181d6ac17c238a12523ebdcc175c7e414bb
Dominik Procházka <dominikprochazka100@gmail.com>
2024-04-25 18:12:22 +0200

    Formatting changes
    hectic usage of single and double quotes causes issues on older python versions.
    plus dropped $ prefixes from discord properties payload

--- config_generator.py
+++ config_generator.py
@@ -1,124 +1,126 @@
 import os, json, platform, subprocess
-supported_platforms = ['Linux', 'Windows']
+supported_platforms = ["Linux", "Windows"]
 current_platform = platform.system()
 if current_platform not in supported_platforms:
-  print('Platform not supported!', current_platform)
-  exit(0)
+    print("Platform not supported!", current_platform)
+    exit(0)
 current_path = os.path.dirname(os.path.abspath(__file__))
 config_path = os.path.join(current_path, 'config.json')
-python_package = 'python3' if current_platform == 'Linux' else 'python'
+python_package = "python3" if current_platform == "Linux" else "python"
 config_dict = {}
 
 # Covering all the possible arch, aarch variations
-def is_arm32():
-    return True if ('aarch' in platform.machine().lower() or 'arm' in platform.machine().lower()) and ('32' in platform.machine().lower() or 'v7' in platform.machine().lower()) else False
-
 def is_arm64():
-    return True if ('aarch' in platform.machine().lower() or 'arm' in platform.machine().lower()) and ('64' in platform.machine().lower() or 'v8' in platform.machine().lower()) else False
+    return True if "aarch64" in platform.machine().lower() or "armv8" in platform.machine().lower() else False
+
+def is_arm32():
+    return True if "aarch" in platform.machine().lower() or "arm" in platform.machine().lower() else False
 
 def is_node_installed():
     try:
-        subprocess.run(['node', '--version'], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        subprocess.run(["node", "--version"], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         return True
     except subprocess.CalledProcessError:
         return False
-      
+            
 def is_git_installed():
     try:
-        subprocess.run(['git', '--version'], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        subprocess.run(["git", "--version"], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         return True
     except subprocess.CalledProcessError:
         return False 
 
-if input('Is jellyfin-rpc installed?(y/n)\n') == 'y':
-  installed_by_installer = input('Did you use jellyfin-rpc installer to install it? (y/n)\n')
-  if installed_by_installer == 'y':
-    if input('Is it installed as a service?(y/n)\n') == 'y':
-      jellyfin_path = 'skip'
+def config():
+    if input("Is jellyfin-rpc installed?(y/n)\n") == "y":
+        installed_by_installer = input("Did you use jellyfin-rpc installer to install it? (y/n)\n")
+        if installed_by_installer == "y":
+            if input("Is it installed as a service?(y/n)\n") == "y":
+                jellyfin_path = "skip"
+            else:
+                if current_platform == "Linux":
+                    jellyfin_path = os.environ['HOME'] + ".local/bin/jellyfin-rpc"
+                else:
+                    jellyfin_path = os.environ['APPDATA'] +    os.path.join('jellyfin-rpc', 'jellyfin-rpc.exe')
+        else:
+            jellyfin_path = rf"{input('Where is your jellyfin rpc located?\n')}"
     else:
-      if current_platform == 'Linux':
-        jellyfin_path = os.environ["HOME"] + ".local/bin/jellyfin-rpc"
-      else:
-        jellyfin_path = os.environ["APPDATA"] + 'jellyfin-rpc\\' + 'jellyfin-rpc.exe'
-  else:
-    jellyfin_path = input('Where is your jellyfin rpc located?\n')
-    if "\"" in jellyfin_path:
-      jellyfin_path = jellyfin_path.replace("\"", "")
-else:
-  if input('Jellyfin-rpc is required, install now?(y/n)\n') == 'y':
-    if input('Install automatically?(y/n)\n') == 'y':
-      subprocess.run(['curl', '-o', os.path.join(current_path, 'installer.py'), '-L', 'https://raw.githubusercontent.com/Radiicall/jellyfin-rpc/main/scripts/installer.py'], shell=True)
-      if current_platform == 'Linux':
-        subprocess.run(['chmod', '+x', 'installer.py'])
-      subprocess.run([python_package, os.path.join(current_path, 'installer.py')])
-      if current_platform == 'Linux':
-        jellyfin_path = os.environ["HOME"] + ".local/bin/jellyfin-rpc"
-      else:
-        jellyfin_path = os.environ["APPDATA"] + 'jellyfin-rpc\\' + 'jellyfin-rpc.exe'
-    else:
-      print('Manual install requires you to create the config manually, please follow steps here: [https://github.com/Radiicall/jellyfin-rpc/wiki/Installation]')
-      print('Downloading latest binary...')
-      if current_platform == 'Linux':
-        if is_arm32():
-          rpc_binary = "jellyfin-rpc-arm32-linux"
-        elif is_arm64():
-          rpc_binary = "jellyfin-rpc-arm64-linux"
-        else :
-          rpc_binary = "jellyfin-rpc-x86_64-linux"
-      else:
-        rpc_binary = 'jellyfin-rpc.exe'
-      subprocess.run(["curl", "-o", os.path.join(current_path, rpc_binary), "-L", f"https://github.com/Radiicall/jellyfin-rpc/releases/latest/download/{rpc_binary}"], shell=True)
-      jellyfin_path = os.path.join(current_path, rpc_binary)
-  else:
-    print("Config generation can't continue without it, exiting...")
-    exit(0)
-config_dict['jellyfin_rpc_path'] = rf"{jellyfin_path}"
-config_dict['node_path'] = os.path.join(current_path, 'arrpc', 'src')
-config_dict['token'] = input('Please input your discord token [https://github.com/Pr0chy/jellyfin-rpc-discord-bridge/wiki/How-do-i-get-the-discord-token%3F]\n')
-print('Choose your RPC style:')
-style1 = 'Jellyfin\n'
-style1 += 'Anime title\n'
-style1 += 'S01 - E01 Episode name\n'
-style1 += '02:03 left\n'
-style2 = 'Anime title\n'
-style2 += 'S01 - E01 Episode name\n'
-style2 += 'Streaming on Jellyfin™\n'
-print(f'------------\n(1)\n{style1}------------\n(2)\n{style2}------------')
-print("So, what's your choice?")
-config_dict['rpc_style'] = int(input()) - 1
-app_title = 'Jellyfin' if config_dict['rpc_style'] == 0 else 'Anime title'
-print('Choose your RPC type:')
-rpc1 = f'Playing a game\n'
-rpc1 += style1
-rpc2 = f'Watching {app_title}\n' #superior in everyway fr
-rpc2 += style2
-print(f'------------\n(1)\n{rpc1}------------\n(2)\n{rpc2}------------')
-print("So, what's your choice?")
-config_dict['rpc_type'] = int(input()) - 1
-with open(config_path, "w") as file:
-    json.dump(config_dict, file, indent=4)
-print('Config generation completed, installing required files now...')
-subprocess.run(['pip', 'install', 'websocket-client'], shell=True)
-if not is_node_installed:
-  if current_platform == 'Linux':
-    try:
-      subprocess.run(['curl', '-sL', 'https://deb.nodesource.com/setup_20.x', '|', 'sudo', '-E', 'bash', '-'], shell=True)
-      subprocess.run(['sudo', 'apt-get', 'install', '-y', 'nodejs'], shell=True)
-    except:
-      print('Failed installing node.js, please install manually! [https://nodejs.org/en/download]')
-  else:
-    print('Node.js not found, please install it! [https://nodejs.org/en/download]')
-if not is_git_installed:
-  if current_platform == 'Linux':
-    try:
-      subprocess.run(['sudo', 'apt-get', 'install', '-y', 'git'], shell=True)
-    except:
-      print('Failed installing git, please install manually! [https://git-scm.com/download/linux]')
-  else:
-    try:
-      subprocess.run(['winget', 'install', '--id', 'Git.Git', '-e', '--source', 'winget'], shell=True)
-    except:
-      print('Failed installing git, please install manually! [https://git-scm.com/download/win]')
-print('Cloning arRPC...')
-subprocess.run(['git', 'clone', 'https://github.com/OpenAsar/arrpc', config_dict['node_path'].removesuffix("src")], shell=True)
-subprocess.run(['npm', 'install'], shell=True, cwd=config_dict['node_path'].removesuffix("src"))
+        if input("Jellyfin-rpc is required, install now?(y/n)\n") == "y":
+            if input("Install automatically?(y/n)\n") == "y":
+                subprocess.run(["curl", "-o", os.path.join(current_path, 'installer.py'), "-L", "https://raw.githubusercontent.com/Radiicall/jellyfin-rpc/main/scripts/installer.py"], shell=True)
+                if current_platform == "Linux":
+                    subprocess.run(["chmod", "+x", 'installer.py'])
+                subprocess.run([python_package, os.path.join(current_path, 'installer.py')])
+                if current_platform == "Linux":
+                    jellyfin_path = os.environ['HOME'] + ".local/bin/jellyfin-rpc"
+                else:
+                    jellyfin_path = os.environ['APPDATA'] + os.path.join('jellyfin-rpc', 'jellyfin-rpc.exe')
+            else:
+                print("Manual install requires you to create the config manually, please follow steps here: [https://github.com/Radiicall/jellyfin-rpc/wiki/Installation]")
+                print("Downloading latest binary...")
+                if current_platform == "Linux":
+                    if is_arm32():
+                        rpc_binary = "jellyfin-rpc-arm32-linux"
+                    elif is_arm64():
+                        rpc_binary = "jellyfin-rpc-arm64-linux"
+                    else :
+                        rpc_binary = "jellyfin-rpc-x86_64-linux"
+                else:
+                    rpc_binary = "jellyfin-rpc.exe"
+                subprocess.run(["curl", "-o", os.path.join(current_path, rpc_binary), "-L", f"https://github.com/Radiicall/jellyfin-rpc/releases/latest/download/{rpc_binary}"], shell=True)
+                jellyfin_path = os.path.join(current_path, rpc_binary)
+        else:
+            print("Config generation can't continue without it, exiting...")
+            exit(0)
+    config_dict['jellyfin_rpc_path'] = rf"{jellyfin_path}"
+    config_dict['node_path'] = os.path.join(current_path, 'arrpc', 'src')
+    config_dict['token'] = input("Please input your discord token [https://github.com/prochy-exe/jellyfin-rpc-discord-bridge/wiki/How-do-i-get-the-discord-token%3F]\n")
+    print("Choose your RPC style:")
+    style1 = "Jellyfin\n"
+    style1 += "Anime title\n"
+    style1 += "S01 - E01 Episode name\n"
+    style1 += "02:03 left\n"
+    style2 = "Anime title\n"
+    style2 += "S01 - E01 Episode name\n"
+    style2 += "Streaming on Jellyfin™\n"
+    print(f"------------\n(1)\n{style1}------------\n(2)\n{style2}------------")
+    print("So, what's your choice?")
+    config_dict['rpc_style'] = int(input()) - 1
+    app_title = "Jellyfin" if config_dict['rpc_style'] == 0 else "Anime title"
+    print("Choose your RPC type:")
+    rpc1 = f"Playing a game\n"
+    rpc1 += style1
+    rpc2 = f"Watching {app_title}\n" #superior in everyway fr
+    rpc2 += style2
+    print(f"------------\n(1)\n{rpc1}------------\n(2)\n{rpc2}------------")
+    print("So, what's your choice?")
+    config_dict['rpc_type'] = int(input()) - 1
+    with open(config_path, "w") as file:
+            json.dump(config_dict, file, indent=4)
+    print("Config generation completed, installing required files now...")
+    subprocess.run(["pip", "install", "websocket-client"], shell=True)
+    if not is_node_installed:
+        if current_platform == "Linux":
+            try:
+                subprocess.run(["curl", "-sL", "https://deb.nodesource.com/setup_20.x", "|", "sudo", "-E", "bash", "-"], shell=True)
+                subprocess.run(["sudo", "apt-get", "install", "-y", "nodejs"], shell=True)
+            except:
+                print("Failed installing node.js, please install manually! [https://nodejs.org/en/download]")
+        else:
+            print("Node.js not found, please install it! [https://nodejs.org/en/download]")
+    if not is_git_installed:
+        if current_platform == "Linux":
+            try:
+                subprocess.run(["sudo", "apt-get", "install", "-y", "git"], shell=True)
+            except:
+                print("Failed installing git, please install manually! [https://git-scm.com/download/linux]")
+        else:
+            try:
+                subprocess.run(["winget", "install", "--id", "Git.Git", "-e", "--source", "winget"], shell=True)
+            except:
+                print("Failed installing git, please install manually! [https://git-scm.com/download/win]")
+    print("Cloning arRPC...")
+    subprocess.run(["git", "clone", "https://github.com/OpenAsar/arrpc", config_dict['node_path'].removesuffix("src")], shell=True)
+    subprocess.run(["npm", "install"], shell=True, cwd=config_dict["node_path"].removesuffix("src"))
+
+if __name__ == "__main__":
+    config()
--- discord_bridge.py
+++ discord_bridge.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
 
-import copy, json, websocket, threading, requests, time, signal, sys, os, subprocess
+import copy, json, websocket, threading, requests, time, signal, sys, os, subprocess, platform
 
 # ANSI escape codes for text colors
 class Color:
-    RED = '\033[91m'
-    GREEN = '\033[92m'
-    CYAN = '\033[96m'
-    RESET = '\033[0m'  # Reset color to default
+    RED = "\033[91m"
+    GREEN = "\033[92m"
+    CYAN = "\033[96m"
+    RESET = "\033[0m"  # Reset color to default
+
+current_platform = platform.system().lower()
 
 def printinfo(text):
   print(Color.CYAN + text + Color.RESET)
@@ -29,19 +31,19 @@ ws_connected = True
 current_path = os.path.dirname(os.path.abspath(__file__))
 config_path = os.path.join(current_path, 'config.json')
 if os.path.exists(config_path):
-  with open(config_path, 'r') as f:
+  with open(config_path, "r") as f:
     # Load the JSON data
     config = json.load(f)
 else:
-  printerr('Please generate config first!')
+  printerr("Please generate config first!")
   exit(0)
 
 params = {}
 for i in range(1, 4):
-  try:
-    params[i] = sys.argv[i]
-  except:
-    params[i] = None
+    try:
+        params[i] = sys.argv[i]
+    except:
+        params[i] = None
 token = params[1] if params[1] else config['token']
 type_ = params[2] if params[2] else config.get('rpc_type', 0)
 rpc_style = params[3] if params[3] else config.get('rpc_style', 0)
@@ -50,41 +52,41 @@ jellyfin_rpc_path = config['jellyfin_rpc_path']
 type_ = 0 if type_ == 0 else 3 #Convert type ints to ones used by Discord (0: Gaming, 3: Watching)
 
 init_payload = {
-    "op": 2,
-    "d": {
-        "token": token,
-        "intents": 0,
-        "properties": {
-            "$os": 'linux',
-            "$browser": 'npm',
-            "$device": 'npm'
+    'op': 2,
+    'd': {
+        'token': token,
+        'intents': 0,
+        'properties': {
+            'os': current_platform,
+            'browser': "python-rpc-bridge",
+            'device': "python-rpc-bridge"
         },
     }
 }
 
 clear_payload = {
-    "op": 3,
-    "d": {
-        "since": 91879201,
-        "activities": None,
-        "status": original_status,
-        "afk": False
+    'op': 3,
+    'd': {
+        'since': 91879201,
+        'activities': None,
+        'status': original_status,
+        'afk': False
     }
 }
 
 def process_image(image_url, type_, app_id):
     global cached_big_image, cached_small_image, new_big_image, new_small_image
     if image_url == cached_big_image or image_url == cached_small_image:
-        return new_big_image if type_ == 'large' else new_small_image
+        return new_big_image if type_ == "large" else new_small_image
     else:
-        url = f'https://discord.com/api/v9/applications/{app_id}/external-assets'
-        response = requests.post(url, headers={'Authorization': token, 'Content-Type': 'application/json'}, json={"urls": [image_url]})
+        url = f"https://discord.com/api/v9/applications/{app_id}/external-assets"
+        response = requests.post(url, headers={'Authorization': token, 'Content-Type': "application/json"}, json={'urls': [image_url]})
         data = response.json()[0]
-        if type_ == 'large':
+        if type_ == "large":
             cached_big_image = image_url
             new_big_image = f"mp:{data['external_asset_path']}"
             return new_big_image
-        elif type_ == 'small':
+        elif type_ == "small":
             cached_small_image = image_url
             new_small_image = f"mp:{data['external_asset_path']}"
             return new_small_image
@@ -98,37 +100,35 @@ def process_data(data):
     elif rpc_style == 1:
         processed_data['name'] = processed_data['details']
         del processed_data['details']
-        processed_data['assets']['large_text'] = 'Streaming on Jellyfin™'
-
+        processed_data['assets']['large_text'] = "Streaming on Jellyfin™"
     if processed_data['assets'].get('large_image'):
         updated_large_image = process_image(processed_data['assets']['large_image'], 'large', processed_data['application_id'])
         while not updated_large_image:
-          time.sleep(1)
-          updated_large_image = process_image(processed_data['assets']['large_image'], 'large', processed_data['application_id'])
+            time.sleep(1)
+            updated_large_image = process_image(processed_data['assets']['large_image'], 'large', processed_data['application_id'])
         processed_data['assets']['large_image'] = updated_large_image
     if processed_data['assets'].get('small_image'):
         updated_small_image = process_image(processed_data['assets']['small_image'], 'small', processed_data['application_id'])
         while not updated_small_image:
-          time.sleep(1)
-          updated_small_image = process_image(processed_data['assets']['small_image'], 'small', processed_data['application_id'])
+            time.sleep(1)
+            updated_small_image = process_image(processed_data['assets']['small_image'], 'small', processed_data['application_id'])
         processed_data['assets']['small_image'] = updated_small_image
-
     return processed_data
 
 def update_status(activity):
     payload = {
-        "op": 3,
-        "d": {
-            "since": 91879201,
-            "activities": [activity],
-            "status": original_status,
-            "afk": False
+        'op': 3,
+        'd': {
+            'since': 91879201,
+            'activities': [activity],
+            'status': original_status,
+            'afk': False
         }
     }
     printinfo(
               ("Updating RPC with:\n" +
               activity['name'] + 
-              (f' {Color.RED}({activity["assets"]["small_text"]}){Color.RESET}' if activity.get("assets", {}).get("small_text") else ""))
+              (f" {Color.RED}({activity['assets']['small_text']}){Color.RESET}" if activity.get('assets', {}).get('small_text') else ""))
               if activity else json.dumps(payload, indent=4)
             ) # COLORS!1!1!
     ws.send(json.dumps(payload))
@@ -138,7 +138,7 @@ def are_objects_equal(obj1, obj2):
     return result
 
 def clear_rpc():
-    printsucc('Clearing RPC')
+    printsucc("Clearing RPC")
     ws.send(json.dumps(clear_payload))
 
 def on_message(ws, message):
@@ -154,15 +154,15 @@ def on_message(ws, message):
 
     if t == "READY":
         original_status = payload['d']['user_settings']['status']
-        printsucc('WebSocket connected, listening for RPC calls')
+        printsucc("WebSocket connected, listening for RPC calls")
         websocket_event()
 
 def heartbeat(interval):
     global ws_connected, previous_s
     while ws_connected:
         payload = {
-            "op": 1,
-            "d": previous_s
+            'op': 1,
+            'd': previous_s
         }
         ws.send(json.dumps(payload))
         printinfo("Sent heartbeat")
@@ -171,7 +171,7 @@ def heartbeat(interval):
 
 def on_error(ws, error):
     if str(error) != "0":
-      printerr(f"Error in Gateway WebSocket: {error}")
+        printerr(f"Error in Gateway WebSocket: {error}")
 
 def on_close(ws, close_status, close_msg):
     global ws_connected
@@ -188,23 +188,23 @@ def on_message_discord(ws, message):
     global previous_payload
     received_payload = json.loads(message)
     try:
-      del received_payload['pid']
-      del received_payload['socketId']
+        del received_payload['pid']
+        del received_payload['socketId']
     except:
-      pass
+        pass
     if received_payload['activity']:
         cleaned_payload = copy.deepcopy(received_payload)
         try:
-          del cleaned_payload['activity']['timestamps'] # While checking if payloads are same, drop timestamps because deviation is only a couple of ms
+            del cleaned_payload['activity']['timestamps'] # While checking if payloads are same, drop timestamps because deviation is only a couple of ms
         except:
-          pass
+            pass
         if not are_objects_equal(cleaned_payload, previous_payload):
             previous_payload = copy.deepcopy(cleaned_payload)
             try:
                 processed_data = process_data(cleaned_payload if type_ == 3 else received_payload)# If type is 0, we need timetamps to show time remaining
                 update_status(processed_data)
             except Exception as e:
-                printerr(f'Error: {e}')
+                printerr(f"Error: {e}")
 
 def on_error_discord(ws, error):
     if str(error) != "0":
@@ -218,11 +218,11 @@ def websocket_event():
     ws_discord.run_forever()
 
 def close_connections():
-  global ws, ws_discord
-  ws.close()
-  printinfo("Gateway WebSocket closed")
-  ws_discord.close()
-  printinfo("Discord WebSocket closed")
+    global ws, ws_discord
+    ws.close()
+    printinfo("Gateway WebSocket closed")
+    ws_discord.close()
+    printinfo("Discord WebSocket closed")
 
 def signal_handler(sig, frame):
     global ws, ws_discord
@@ -236,14 +236,14 @@ def signal_handler(sig, frame):
     os._exit(0)
 
 def start_node_server():
-    subprocess.run(['node', node_server_path], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    subprocess.run(["node", node_server_path], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
 def start_jellyfin_rpc():
     process = subprocess.Popen(jellyfin_rpc_path, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     while process.poll() is None:
-      for line in iter(process.stdout.readline, b''):
-        decoded_line = line.decode('utf-8')
-        if 'Cleared' in decoded_line: clear_rpc() # Clear RPC when jellyfin-rpc clears it
+      for line in iter(process.stdout.readline, b""):
+        decoded_line = line.decode("utf-8")
+        if "Cleared" in decoded_line: clear_rpc() # Clear RPC when jellyfin-rpc clears it
 
 if __name__ == "__main__":
     signal.signal(signal.SIGINT, signal_handler)
@@ -253,13 +253,13 @@ if __name__ == "__main__":
     time.sleep(5)
     jellyfin_rpc_thread.start()
   
-    ws = websocket.WebSocketApp('wss://gateway.discord.gg/?v=10&encoding=json',
+    ws = websocket.WebSocketApp("wss://gateway.discord.gg/?v=10&encoding=json",
                                 on_message=on_message,
                                 on_error=on_error,
                                 on_close=on_close)
     ws.on_open = on_open
 
-    ws_discord = websocket.WebSocketApp('ws://127.0.0.1:1337',
+    ws_discord = websocket.WebSocketApp("ws://127.0.0.1:1337",
                                         on_message=on_message_discord,
                                         on_error=on_error_discord,
                                         on_close=on_close_discord)
